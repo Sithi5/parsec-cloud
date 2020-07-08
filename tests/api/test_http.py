4bba9fefcba0069f70f0995f460e037f045df614
Sithi5 <ma.sithis@gmail.com>
2020-07-08 18:50:04 +0200

    Revert "removing test to test CI"
    This reverts commit b9024cb2f8947a1b1c14ca536a5add9b500d6d79.

---
+++ tests/api/test_http.py
@@ -0,0 +1,54 @@
+# Parsec Cloud (https://parsec.cloud) Copyright (c) AGPLv3 2019 Scille SAS
+
+import pytest
+import trio
+
+
+@pytest.mark.trio
+async def test_send_http_request(running_backend):
+    stream = await trio.open_tcp_stream(running_backend.addr.hostname, running_backend.addr.port)
+    await stream.send_all(
+        b"GET / HTTP/1.1\r\n"
+        b"Host: parsec.example.com\r\n"
+        b"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0\r\n"
+        b"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n"
+        b"Accept-Language: fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3\r\n"
+        b"Accept-Encoding: gzip, deflate\r\n"
+        b"DNT: 1\r\n"
+        b"Connection: keep-alive\r\n"
+        b"Upgrade-Insecure-Requests: 1\r\n"
+        b"Cache-Control: max-age=0\r\n"
+        b"\r\n"
+    )
+    rep = await stream.receive_some()
+    print(rep)
+
+
+@pytest.mark.trio
+async def test_send_http_request_to_rest_invite_no_invitation_type(running_backend):
+    stream = await trio.open_tcp_stream(running_backend.addr.hostname, running_backend.addr.port)
+    await stream.send_all(
+        b"GET /api/invite?organization_id=thisistheorganizationid123456789&token=S3CRET&no_ssl=true HTTP/1.0\r\n\r\n"
+    )
+    rep = await stream.receive_some()
+    print(rep)
+    # assert re.search(b"HTTP/1.1 400", rep) is not None
+
+
+@pytest.mark.trio
+async def test_send_http_request_to_rest_false_route(running_backend):
+    stream = await trio.open_tcp_stream(running_backend.addr.hostname, running_backend.addr.port)
+    await stream.send_all(b"GET /api/false HTTP/1.0\r\n\r\n")
+    rep = await stream.receive_some()
+    print(rep)
+    # assert re.search(b"HTTP/1.1 400", rep) is not None
+
+
+@pytest.mark.trio
+async def test_send_http_request_to_rest_invite_valid(running_backend):
+    stream = await trio.open_tcp_stream(running_backend.addr.hostname, running_backend.addr.port)
+    await stream.send_all(
+        b"GET /api/invite?organization_id=thisistheorganizationid123456789&invitation_type=invitation_type&token=S3CRET&no_ssl=true HTTP/1.0\r\n\r\n"
+    )
+    rep = await stream.receive_some()
+    print(rep)
